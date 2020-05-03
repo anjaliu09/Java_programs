9859be0f3ee697d7859b7582862f76f3320555ed
anjaliu09 <anjaliu9994@gmail.com>
2020-05-03 14:34:43 +0000

    Changes done

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class hello{
 public static void main(String args[]){
 System.out.println("Keep Exploring git through command line");
-System. out. println("Session 1 of Jenkins");
+System. out. println("Session :Integration of Git and JEnkins");
 }
 }
